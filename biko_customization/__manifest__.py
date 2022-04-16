5f4060704ee23a7351300871e9f3ed61fedc7d39
Artem Borovlev <bas@qdes.com.ua>
2022-04-16 12:30:49 +0300

    [*] changed sequence for menu CRM/Sales/Teams [+] added new filter "Assigned to me and opened"

---
+++ biko_customization/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "BIKO: customization",
+    "category": "Themes/Backend",
+    "version": "15.0.1.1.1",
+    'author': 'BIKO Solutions',
+    'company': 'BIKO Solutions',
+    'maintainer': 'BIKO Solutions',
+    "depends": ['crm'],
+    "data": [
+        'views/crm_lead_views.xml',
+        'views/biko_custom_menus.xml',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
