b96f60196218c59d1c61e39a1f71decbb724d563
Artem Borovlev <bas@qdes.com.ua>
2022-04-19 18:59:50 +0300

    [*] margine % calc (wrong % at all)

--- biko_customization/models/sale_report.py
+++ biko_customization/models/sale_report.py
@@ -7,5 +7,5 @@ class SaleReport(models.Model):
     margin_percent = fields.Float('Margin (%)')
 
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
-        fields['margin_percent'] = ", SUM(l.margin_percent) AS margin_percent"
+        fields['margin_percent'] = ", SUM(l.margin_percent * 100) AS margin_percent"
         return super(SaleReport, self)._query(with_clause, fields, groupby, from_clause)
