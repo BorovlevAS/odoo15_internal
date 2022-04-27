014c14eab2dfa3ac83d66260c3a96c6601782870
Artem Borovlev <bas@qdes.com.ua>
2022-04-27 22:36:14 +0300

    добавил в сделки свой источник (аналог Б24) и переименовал названия полей

--- biko_additional_fields/models/crm_lead.py
+++ biko_additional_fields/models/crm_lead.py
@@ -4,5 +4,5 @@ class Lead(models.Model):
 
     _inherit = 'crm.lead'
 
-    inner_source_id = fields.Many2one(comodel_name = 'biko.source', string = 'Source')
+    inner_source_id = fields.Many2one(comodel_name = 'biko.source', string = 'Source (QDES)')
     lost_reason_comment = fields.Char(string = 'Lost reason (comment)')
