06b8f93657a504722ff9bfb4b81abcc5b49c9b5b
Artem Borovlev <bas@qdes.com.ua>
2022-05-01 13:21:38 +0300

    сделал кнопку свертки chatter'a

---
+++ biko_hide_pager/static/src/js/hide_panel_toggle.js
@@ -0,0 +1,37 @@
+odoo.define('hide_panel_toggle.HPanel',function (require) {
+"use strict";
+
+	const utils = require('web.utils');
+    var Pager = require('web.Pager');
+    utils.patch(Pager.prototype, "BIKO.hide.panel", {
+
+        _onHideShow: function () {
+
+            let $but = $('.o_pager_hide');
+            let $chatter = $('.o_FormRenderer_chatterContainer');
+
+            if ((!$but.length) || (!$chatter.length)) return;
+
+			if ($chatter.hasClass('o_hidden')) {
+
+				$chatter.removeClass('o_hidden');
+				$but.removeClass('fa-step-backward').addClass('fa-step-forward');
+				$but.attr({
+				    'title': 'Hide Chatter',
+				    'aria-label': 'Hide'
+				});
+
+			} else {
+
+				$chatter.addClass('o_hidden');
+				$but.removeClass('fa-step-forward').addClass('fa-step-backward');
+				$but.attr({
+				    'title': 'Show Chatter',
+				    'aria-label': 'Show'
+				});
+			}
+
+        },
+    });
+
+});
