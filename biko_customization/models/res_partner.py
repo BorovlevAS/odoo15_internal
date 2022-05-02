0c56559c5b041ac2a746e61f319f1390201ca64c
danilm <mdv99.danil@gmail.com>
2022-05-02 21:27:36 +0300

    задача выполнена и протестирована

--- biko_customization/models/res_partner.py
+++ biko_customization/models/res_partner.py
@@ -6,8 +6,3 @@ class Partner(models.Model):
     def _get_contact_name(self, partner, name):
         company_name = partner.commercial_company_name or partner.sudo().parent_id.name
         return "%s, %s" % (name, company_name)
-
-    def action_do_something(self):
-        for partner in self:
-            partner.name = partner.name + "+"
-            partner.name = ''.join(partner.name[0:-1])
