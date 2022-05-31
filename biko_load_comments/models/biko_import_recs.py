23f6420261a8ee7e63bab1d132e161da2a730d6a
Artem Borovlev <bas@qdes.com.ua>
2022-05-31 21:14:18 +0300

    added gitignore

--- biko_load_comments/models/biko_import_recs.py
+++ biko_load_comments/models/biko_import_recs.py
@@ -1,6 +1,8 @@
 from odoo import models, fields
 import base64
 import json
+from datetime import datetime
+import requests
 
 class ImportRecs(models.TransientModel):
     _name = 'biko.import.recs'
@@ -14,8 +16,10 @@ class ImportRecs(models.TransientModel):
         jsdata = json.loads(data)
 
         env_deals = self.env['crm.lead'].env
+        i = 0
 
         for deal in jsdata.values():
+            if i >= 3: break
 
             comments_list = deal['comments']
             # self.env.ref
@@ -25,4 +29,20 @@ class ImportRecs(models.TransientModel):
             external_id = deal['external_id']
             id = deal['id']
 
-            # env_deals.search()
+            record = env_deals.ref('__import__.' + external_id)
+
+            if record:
+                for comment in comments_list.values():
+                    date_time = datetime.fromisoformat(comment['CREATED']).replace(tzinfo=None)
+                    msg = comment['COMMENT']
+                    f_attachments = []
+                    # TODO: заполнить тут прикрепленные файлы и передать их в функцию
+                    if ('FILES' in comment.keys()):
+                        i += 1
+                        for c_file in comment['FILES'].values():
+                            f_name = c_file['name']
+                            req = requests.get(c_file['urlDownload'])
+                            f_attachments.append((f_name, req.content))
+
+                    message_rec = record.message_post(body=msg, message_type='comment', attachments=f_attachments)
+                    message_rec['date'] = date_time
